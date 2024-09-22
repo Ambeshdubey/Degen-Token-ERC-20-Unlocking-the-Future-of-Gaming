8f94d15ba09198dcbf462f9cdaff7677a6797fd4
Ambeshdubey <153484734+Ambeshdubey@users.noreply.github.com>
2024-09-22 16:23:24 +0530

    DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -5,16 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("Degen", "DGN") Ownable(initialOwner) {}
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        // Assign initial ownership to deployer (msg.sender)
+    }
 
+    // Mint tokens to a specific address. Only the owner can mint.
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
+    // Burn tokens from the sender's account
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
+    // Redeem tokens from a player's account. Only the owner can redeem tokens.
     function redeem(address from, uint256 amount) public onlyOwner {
         _transfer(from, owner(), amount);
     }
